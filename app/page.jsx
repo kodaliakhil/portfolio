fe148eda1c925944726d7cd6740e8177dda7c373
kodaliakhil <kodaliakhil1999@gmail.com>
2024-06-03 17:59:00 +0530

    photo and roatating circle

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Download } from "lucide-react";
 // components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb:24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />
@@ -38,7 +39,9 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
